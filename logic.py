94d3acba1f69e48b7845973b1288bddca0e2cb7d
Skulldunno <fregennick@gmail.com>
2026-01-14 14:17:28 +0100

    added jsonladen und aufgaben_titel_auflisten Funktionen

--- logic.py
+++ logic.py
@@ -1,7 +1,18 @@
 #logic for Rechtschreib-Tool
+import json
 
+aufgabenListe = [] #leere Aufgabenliste wird erstellt
 
+#die aufgaben.json datei wird geladen und alle Inhalte in die Aufgabenliste gesteckt
+def jsonladen():
+    with open ("aufgaben.json","r") as f:
+        global aufgabenListe
+        aufgabenListe = json.load(f)
 
-def uebung_erstellen():
-    pass
+#listet alle Titel der Aufgaben auf
+def aufgaben_titel_auflisten():
+    for aufgaben in aufgabenListe:
+        print(f"Aufgabe:{aufgaben["Titel"]}")
 
+jsonladen()
+aufgaben_titel_auflisten()
