60774e2e984b051e93a3a334e7b2d84edcb325e5
Skulldunno <fregennick@gmail.com>
2026-01-14 15:11:29 +0100

    added a function to loigc.py to list all titel and aufgabenbeschreibungen

--- logic.py
+++ logic.py
@@ -5,7 +5,7 @@ aufgabenListe = [] #leere Aufgabenliste wird erstellt
 
 #die aufgaben.json datei wird geladen und alle Inhalte in die Aufgabenliste gesteckt
 def jsonladen():
-    with open ("aufgaben.json","r") as f:
+    with open ("aufgaben.json","r",encoding= "utf-8" ) as f:
         global aufgabenListe
         aufgabenListe = json.load(f)
 
@@ -14,5 +14,10 @@ def aufgaben_titel_auflisten():
     for aufgaben in aufgabenListe:
         print(f"Aufgabe:{aufgaben["Titel"]}")
 
+def aufgaben_auflisten():
+    for aufgaben in aufgabenListe:
+        print(f"Aufgabe: {aufgaben['Titel']} \nAufgabenbeschreibung: {aufgaben['_aufgabenbeschreibung']} \nAufgabenbeschreibung2: {aufgaben['Aufgabenbeschreibung']} ")
+
 jsonladen()
-aufgaben_titel_auflisten()
+#aufgaben_titel_auflisten()
+aufgaben_auflisten()
