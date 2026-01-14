cfc223dd5299edb7fec34c9dda62bc2f384f05b2
Zerberstian <248627354+Zerberstian@users.noreply.github.com>
2026-01-14 14:37:34 +0100

    adding position Argument to the Window

--- GUI_new.py
+++ GUI_new.py
@@ -3,12 +3,17 @@ from tkinter import *       #import für tkinter
 #def en Fenster
 window = Tk()
 
+window.update_idletasks()
+
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
 # Set window size to screen size
-window.geometry(f"{screen_width}x{screen_height}+0+0")
-window.attributes("-fullscreen", False)  #fullsc
+x_pos = -9 #stat point of Window
+y_pos = 0
+window.geometry(f"{screen_width}x{screen_height}+{x_pos}+{y_pos}")
+
+window.attributes("-fullscreen", False)  #fullscreen
 window.bind("<Escape>", lambda e: window.attributes("-fullscreen", False)) #Escape fullscreen exit
 window.bind("<F12>", lambda e: window.attributes("-fullscreen", True))  #F12 fullscreen toggle
 window.title("Rechtschreib-Tool")   #changing Title from tk to Rechtschreib-Tool
@@ -25,8 +30,9 @@ window.iconphoto(True, icon)
 label1 = Label(window, image=icon)
 label1.place(x=0, y=0)
 
-label2 = Label(window, text="Hallo",    #adding a big Title
-               font=("Comic Sans", 30),
+label2 = Label(window,
+               text="Hallo",    #adding a big Title
+               font=("Ariel", 30),
                bg="#E0470A",
                fg="#ffffff")
 
