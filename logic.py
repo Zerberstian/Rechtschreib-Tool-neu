63f61124d5a40c4d196468c97ec9e350999628bc
Skulldunno <fregennick@gmail.com>
2026-01-15 07:43:45 +0100

    added a function to open and answer questions in the console

--- logic.py
+++ logic.py
@@ -16,8 +16,28 @@ def aufgaben_titel_auflisten():
 
 def aufgaben_auflisten():
     for aufgaben in aufgabenListe:
-        print(f"Aufgabe: {aufgaben['Titel']} \nAufgabenbeschreibung: {aufgaben['_aufgabenbeschreibung']} \nAufgabenbeschreibung2: {aufgaben['Aufgabenbeschreibung']} ")
+        print(f"Aufgabe: {aufgaben['Titel']} \nAufgabenbeschreibung: {aufgaben['_aufgabenbeschreibung']} \nAufgabenbeschreibung: {aufgaben['Aufgabenbeschreibung']} ")
+
+def aufgabe_open():
+    fehler = 0
+    richtig = True
+    for aufgaben in aufgabenListe:
+        for uebung in aufgaben['UebungenListe']:
+            x = 0
+            if richtig:
+                for i in uebung['Moeglichkeiten']:
+                    print(i)
+                antwort = int(input('1, 2 oder 3?'))
+                if antwort == uebung['KorrekteAntwort']:
+                    pass
+                else:
+                    fehler +=1
+                x+=1
+                if x == 10:
+                    break
+    print(f"Fehler {fehler}")
 
 jsonladen()
 #aufgaben_titel_auflisten()
-aufgaben_auflisten()
+#aufgaben_auflisten()
+aufgabe_open()
