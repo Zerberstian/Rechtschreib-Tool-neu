f91d32b35a3aa45fc369a812965ec9d75ecdb6c9
Skulldunno <fregennick@gmail.com>
2026-01-15 08:33:10 +0100

    added list special question, list uebungsbereich, translatet comments to english

--- logic.py
+++ logic.py
@@ -1,23 +1,25 @@
 #logic for Rechtschreib-Tool
 import json
 
-aufgabenListe = [] #leere Aufgabenliste wird erstellt
+aufgabenListe = [] #Create an empty list for questions
 
-#die aufgaben.json datei wird geladen und alle Inhalte in die Aufgabenliste gesteckt
+#Function to load aufgaben.json
 def jsonladen():
     with open ("aufgaben.json","r",encoding= "utf-8" ) as f:
         global aufgabenListe
         aufgabenListe = json.load(f)
 
-#listet alle Titel der Aufgaben auf
+#Function to list all titel
 def aufgaben_titel_auflisten():
     for aufgaben in aufgabenListe:
         print(f"Aufgabe:{aufgaben["Titel"]}")
 
+#Function to list all titel with description
 def aufgaben_auflisten():
     for aufgaben in aufgabenListe:
         print(f"Aufgabe: {aufgaben['Titel']} \nAufgabenbeschreibung: {aufgaben['_aufgabenbeschreibung']} \nAufgabenbeschreibung: {aufgaben['Aufgabenbeschreibung']} ")
 
+#Function to show a question inside the console , answer wrong and the programm will close
 def aufgabe_open():
     fehler = 0
     richtig = True
@@ -37,7 +39,31 @@ def aufgabe_open():
                     break
     print(f"Fehler {fehler}")
 
+#Function to choose a Questions
+def aufgabe_auswahl():
+    for aufgaben in aufgabenListe:
+        pass
+
+#Function to show Questions with status "IstSpeziell" True
+def spezial_aufgaben():
+    for aufgaben in aufgabenListe:
+        if aufgaben['IstSpeziell']:
+            for uebung in aufgaben['UebungenListe']:
+                print(uebung)
+
+#Function to list all "uebungsbereich"
+def uebungsbereich_auflisten():
+    uebungsbereichListe = []
+    for uebungsbereich in aufgabenListe:
+        if uebungsbereich['Uebungsbereich'] in uebungsbereichListe:
+            pass
+        else:
+            uebungsbereichListe.append(uebungsbereich['Uebungsbereich'])
+    print(uebungsbereichListe)
+
 jsonladen()
 #aufgaben_titel_auflisten()
 #aufgaben_auflisten()
-aufgabe_open()
+#aufgabe_open()
+#spezial_aufgaben()
+uebungsbereich_auflisten()
