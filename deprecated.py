d751d058bca89bad7910a6745a4949dd7e76d5fb
Skulldunno <fregennick@gmail.com>
2026-01-16 10:54:42 +0100

    changed func dict_questiontitel() to now print all topics(bereiche) and a list of their respective titel, return a dict bereichdict

---
+++ deprecated.py
@@ -0,0 +1,62 @@
+#Function to list all titel
+def aufgaben_titel_auflisten():
+    for aufgaben in aufgabenListe:
+        print(f"Aufgabe:{aufgaben["Titel"]}")
+
+#Function to list all titel with description
+def aufgaben_auflisten():
+    for aufgaben in aufgabenListe:
+        print(f"Aufgabe: {aufgaben['Titel']} \nAufgabenbeschreibung: {aufgaben['_aufgabenbeschreibung']} \nAufgabenbeschreibung: {aufgaben['Aufgabenbeschreibung']} ")
+
+#Function to choose Übungsbereich
+def uebungsbereich_auswahl():
+    while True:
+        uebungsbereichListe = uebungsbereich_auflisten()
+        auswahl =int(input("1, 2, 3, 4, 5 oder 6?"))
+        auswahl-=1
+        if auswahl < 6 and auswahl >= 0:
+            uebungsbereich = uebungsbereichListe[auswahl]
+            print(uebungsbereich)
+            if uebungsbereich not in bereichListe:
+                bereichListe.append(uebungsbereich)
+            else:
+                print("Bereits ausgewählt")
+            break
+        else:
+            print("Ungültiger Bereich")
+    while True:
+        nochmal = input("Noch ein Übungsbereich auswählen? Y/N\n")
+        if nochmal.lower() == "y" :
+            uebungsbereich_auswahl()
+        elif nochmal.lower() == "n":
+            break
+        else:
+            print('You Stupid')
+    print(f"Die Ausgewählten Übungsbereiche sind {bereichListe}")
+
+#Function to show a question inside the console , answer wrong and the programm will close
+def aufgabe_open():
+    fehler = 0
+    richtig = True
+    for aufgaben in aufgabenListe:
+        for uebung in aufgaben['UebungenListe']:
+            x = 0
+            if richtig:
+                for i in uebung['Moeglichkeiten']:
+                    print(i)
+                antwort = int(input('1, 2 oder 3?'))
+                if antwort == uebung['KorrekteAntwort']:
+                    pass
+                else:
+                    fehler +=1
+                x+=1
+                if x == 10:
+                    break
+    print(f"Fehler {fehler}")
+
+#Function to show Questions with status "IstSpeziell" True
+def spezial_aufgaben():
+    for aufgaben in aufgabenListe:
+        if aufgaben['IstSpeziell']:
+            for uebung in aufgaben['UebungenListe']:
+                print(uebung)
--- logic.py
+++ logic.py
@@ -10,33 +10,6 @@ def jsonladen():
         global aufgabenListe
         aufgabenListe = json.load(f)
 
-#Function to show a question inside the console , answer wrong and the programm will close
-def aufgabe_open():
-    fehler = 0
-    richtig = True
-    for aufgaben in aufgabenListe:
-        for uebung in aufgaben['UebungenListe']:
-            x = 0
-            if richtig:
-                for i in uebung['Moeglichkeiten']:
-                    print(i)
-                antwort = int(input('1, 2 oder 3?'))
-                if antwort == uebung['KorrekteAntwort']:
-                    pass
-                else:
-                    fehler +=1
-                x+=1
-                if x == 10:
-                    break
-    print(f"Fehler {fehler}")
-
-#Function to show Questions with status "IstSpeziell" True
-def spezial_aufgaben():
-    for aufgaben in aufgabenListe:
-        if aufgaben['IstSpeziell']:
-            for uebung in aufgaben['UebungenListe']:
-                print(uebung)
-
 #Function to list all "uebungsbereich"
 def uebungsbereich_auflisten():
     uebungsbereich_liste = []
