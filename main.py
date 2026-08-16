e3ae071b49c6d0c694e614d5192a376911a84eb6
Feli <84331186+Feligresa@users.noreply.github.com>
2026-08-16 10:40:32 +0200

    VEnv note

--- main.py
+++ main.py
@@ -36,7 +36,5 @@ python -m PyInstaller --onefile --windowed --name "RechtschreibTool" --add-data 
 '''
 python -m venv venv
 venv/Scripts/activate  # on Windows
-pip install PyInstaller
-pip install requests
-pip install matplotlib
+pip install -r requirements.txt
 '''
